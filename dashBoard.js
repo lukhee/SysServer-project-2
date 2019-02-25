5ef7c2833dbe722fbfbbc7aa4da6c45c2e80b722
lukhee <o.balogun@ymail.com>
2019-02-25 22:43:29 +0100

    first dashboard commit

---
+++ dashBoard.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+      $("#on").hide();
+      $("#off").show();
+      $(".hidden-search").hide();
+      $(".sub-list").hide();
+      $(".fa-times").hide();
+      $(".login-signout").hide();
+
+    $("#off").click(function(){
+        $("#on").show();
+        $("#off").hide();
+        $(".side-list-r").css({"display":"none"})
+        $(".sub-list").css({"display":"none"})
+        $(".sidebar").css({"width":"4%"})
+        // $(".li").css({"padding":"20px"})
+      });
+
+      $("#on").click(function(){
+        $("#off").show();
+        $("#on").hide();
+        $(".side-list-r").css({"display":"flex"})
+        $(".sidebar").css({"width":"25%"})
+      });
+
+      $(".search-here").click(function(){
+        $(".hidden-search").toggle();
+        $(".fa-times").toggle();
+        // $(".hidden-search").hide();
+      });
+
+      $(".login-name").click(function(){
+        $(".login-signout").toggle();
+      });
+
+      $(".li").click(function(){
+        $("this").css({"color":"red"})
+        $(".sub-list").toggle();
+      });
+      $(".li").on('click', function() {
+        $(this).addClass("button-clickd");
+    });
+
+    
+
+  });
+
+//   border-left: 4px solid green;
+//   background: #2a3242;
